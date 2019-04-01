646f4821407920e95b5080e0915ad86f51cffcea
iskenxan <iskenxan11@gmail.com>
2019-04-01 11:55:17 -0500

    adjust linkedinRedirect uri

--- src/components/sso/LinkedIn.js
+++ src/components/sso/LinkedIn.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ButtonImage from './linkedin.svg'
 
-const redirectUri = 'http%3A%2F%2Flocalhost%3A8081%2Fmodify%2Flinkedin'
-// const redirectUri = 'https%3A%2F%2Fapi.trustbot.io%2Frest%2Foauth%2Flinkedin';
+const devRedirectUri = 'http%3A%2F%2Flocalhost%3A8081%2Fmodify%2Flinkedin'
+const productionRedirectUri =
+	'https%3A%2F%2Ftrustlayerapi.trustbot.io%2Fmodify%2Flinkedin'
+const redirectUri =
+	process.env.NODE_ENV === 'development'
+		? devRedirectUri
+		: productionRedirectUri
+
 const AUTHORIZE_URI = `https://www.linkedin.com/oauth/v2/authorization?client_id=78bo5ls26ov71s&response_type=code&redirect_uri=${redirectUri}&scope=r_liteprofile%20r_emailaddress&state=`
 
 const linkedInButton = ({ state }) => (
