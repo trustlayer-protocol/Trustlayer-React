c4bd3349dd1e3bef89c46ddf6356f275072ffc77
Albert De Guzman <albertdeguzma@gmail.com>
2019-04-01 11:19:28 -0700

    update hash

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'typeface-roboto'
 import HashReader from './HashReader.js'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Switch, BrowserRouter, Route } from 'react-router-dom'
 import TrustlayerAdopt from 'scenes/TrustlayerAdopt'
 import SsoSuccess from 'scenes/SsoSuccess'
 import styled from '@emotion/styled'
@@ -45,9 +45,11 @@ export default () => (
 				</Link>
 			</Header>
 			<BrowserRouter>
-				<Route exact path="/" component={TrustlayerAdopt} />
-				<Route path="/sso-success" component={SsoSuccess} />
-				<Route exact path="/:hashId" component={HashReader} />
+				<Switch>
+					<Route exact path="/" component={TrustlayerAdopt} />
+					<Route path="/sso-success" component={SsoSuccess} />
+					<Route exact path="/:hashId" component={HashReader} />
+				</Switch>
 			</BrowserRouter>
 		</Root>
 	</>
